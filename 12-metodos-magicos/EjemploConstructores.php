2799c5da0dc729da0b4f77b84e06055c4a11f865
Gorka Muñoz <gorka.muoz@takeachef.com>
2016-12-25 13:05:05 +0100

    Added examples for the magic methods

---
+++ 12-metodos-magicos/EjemploConstructores.php
@@ -0,0 +1,28 @@
+<?php
+
+class BaseClass {
+   function __construct() {
+       print "En el constructor BaseClass\n";
+   }
+}
+
+class SubClass extends BaseClass {
+   function __construct() {
+       parent::__construct();
+       print "En el constructor SubClass\n";
+   }
+}
+
+class OtherSubClass extends BaseClass {
+    // heredando el constructor BaseClass
+}
+
+// En el constructor BaseClass
+$obj = new BaseClass();
+
+// En el constructor BaseClass
+// En el constructor SubClass
+$obj = new SubClass();
+
+// En el constructor BaseClass
+$obj = new OtherSubClass();
